ae5fea724d9cb7764171d73fd8ca64e136fd2db8
bashir <abdirahman.bashir88@gmail.com>
2023-05-14 07:21:27 +0300

    set up driver config and base class

--- src/test/java/com/Base/BaseClass.java
+++ src/test/java/com/Base/BaseClass.java
@@ -1,4 +1,63 @@
 package com.Base;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Properties;
+
 public class BaseClass {
+
+    public static WebDriver driver;
+    public static Properties prop;
+    private static final Logger logger = LoggerFactory.getLogger(BaseClass.class);
+
+    public static void browser(){
+        try{
+            logger.info(">>>>>Before Opening the file<<<<<<<");
+            prop =new Properties();
+            FileInputStream fis = new
+                    FileInputStream("src\\main\\java\\com\\Configuration\\Config.properties");
+            prop.load(fis);
+        }
+        catch (IOException e){e.printStackTrace();}
+        catch (NullPointerException e) {e.printStackTrace();}
+    }
+
+    public static void setUp(){
+        logger.info(">>>>Before opening the browser!<<<<<<");
+        String browser = prop.getProperty("browser");
+        if(browser!=null){
+            switch (browser.toLowerCase()){
+                case "edge":
+                    WebDriverManager.edgedriver().setup();
+                    driver = new EdgeDriver();
+                    break;
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    break;
+                case "ff":
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                default:
+                    logger.error("Invalid driver specification in config file:",browser);
+                    throw new IllegalArgumentException("Invalid driver specification in Config:"+browser);
+            }
+        } else {
+            logger.error("Invalid driver specification in config file:",browser);
+            throw new IllegalArgumentException("Invalid driver specification in Config:"+browser);
+        }
+        driver.get(prop.getProperty("url"));
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+    }
 }
